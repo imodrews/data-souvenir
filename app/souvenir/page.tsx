b5107ec40cac67d6b63b3763969b8cb64b3a03ef
imogendrews <imoleadrews@gmail.com>
2026-09-09 13:55:14 +0200

    Add souvenir page

---
+++ app/souvenir/page.tsx
@@ -0,0 +1,8 @@
+export default function SouvenirPage() {
+  return (
+    <main>
+      <h1>Your Souvenir</h1>
+      <p>Your collected exhibition data will eventually appear here.</p>
+    </main>
+  );
+}
